9bb9fdafd1629ac5c812b8774daf05e21319abf6
filippoziliotto <filippo.ziliotto@phd.unipd.it>
2026-03-24 14:59:55 +0100

    remove url base

--- app/lib/src/config/app_config.dart
+++ app/lib/src/config/app_config.dart
@@ -1,11 +1,6 @@
 class AppConfig {
   AppConfig._();
 
-  static const String apiBaseUrl = String.fromEnvironment(
-    'API_BASE_URL',
-    defaultValue: 'https://YOUR_PRODUCTION_BACKEND_URL',
-  );
-
   // TODO: replace with real Play Store URL after publishing
   static const String playStoreUrl =
       'https://play.google.com/store/apps/details?id=com.fzlabs.campaignforge';
